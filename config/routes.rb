7155586c7d0184a0f904d6e07f945a4ef61e79f1
israel martins <email.israel.br@gmail.com>
2019-03-09 11:31:03 -0400

    upload

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :houses
+  devise_for :users
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :docs
+
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+  
+  root 'welcome#index'
+end
